0e178720a438144fab2afc245f21bac0eb2829cf
Nagananda K K <naganandakk@gmail.com>
2019-09-11 17:43:08 +0530

    - Redux store removed. - manifest.json updated.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { ThemeProvider } from '@material-ui/styles';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reduxThunk from 'redux-thunk';
 
 import App from './App';
-import reducers from './store/reducers';
+import theme from './theme';
 
 import * as serviceWorker from './serviceWorker';
 
-const theme = createMuiTheme({
-    typography: {
-        fontFamily: "'Google Sans', sans-serif"
-    }
-});
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-    reducers,
-    composeEnhancers(
-        applyMiddleware(
-            reduxThunk
-        )
-    )
-)
-
 ReactDOM.render(
     <ThemeProvider theme={theme}>
-        <Provider store={store}>
-            <App />
-        </Provider>
+       <App />
     </ThemeProvider>,
     document.getElementById('root')
 );
